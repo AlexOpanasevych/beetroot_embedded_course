1086b38dd7c90c56e14bdc953869b5a26ca428d8
AlexOpanasevych <karabas2903@gmail.com>
2026-04-18 10:01:27 +0300

    added miniproject_module1

--- module1.6/src/main.cpp
+++ module1.6/src/main.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 
-#define LDR_PIN     9
+#define LDR_PIN     10
 #define ADC_MAX     4095
 #define VREF        3.3f
 #define READ_INTERVAL_MS 100
@@ -8,8 +8,6 @@
 void setup() {
   Serial.begin(115200);
   while (!Serial) delay(10);  // wait for native USB CDC to connect
-  analogReadResolution(12);
-  analogSetPinAttenuation(LDR_PIN, ADC_6db);
 }
 
 void loop() {
@@ -21,7 +19,7 @@ void loop() {
   uint32_t analogReadMV = analogReadMilliVolts(LDR_PIN);
 
   Serial.printf("Raw: %4d | Voltage: %.2fV | Light: %.1f%% | AnalogReadMV: %d | exp: %.1f%\n",
-                raw, voltage, lightPercent, analogReadMV, (1.0f - analogReadMV / (VREF * 1000.0f)) * 100.0f);
+                raw, voltage, lightPercent, analogReadMV, ((analogReadMV / (VREF * 1000.0f)) / voltage) * 100.0f);
 
   delay(READ_INTERVAL_MS);
 }
